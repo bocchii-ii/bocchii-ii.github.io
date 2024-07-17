3d71702b87a8a81182eb71dcd5b3cce20c4e8100
bbaebea <j.renz@mfi.org.ph>
2024-07-17 11:25:42 +0800

    adsf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-beautiful-dnd/",
   plugins: [react()],
-  base: "https://bocchii-ii.github.io/"
 })
